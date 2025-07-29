bacd6987e23cded2e17464a757048ded49d5c995
Arumugam N <nmaru2904@gmail.com>
2025-07-29 15:52:30 +0530

    Implemented local saving and loading

--- easy/Task.java
+++ easy/Task.java
@@ -1,10 +1,18 @@
 public class Task {
+    private static int counter =1;
     private int id;
     private String title;
     private boolean isDone;
 
-    public Task(int id, String title) {
+    public Task(int id, String title,boolean isDone) {
         this.id = id;
+        this.title = title;
+        this.isDone = isDone;
+        counter = Math.max(counter, id +1);
+    }
+
+    public Task(String title){
+        this.id = counter++;
         this.title = title;
         this.isDone = false;
     }
@@ -27,5 +35,17 @@ public class Task {
 
     public void getDetails() {
         System.out.println((isDone ? "[X] " : "[] ") + id + ": " + title + "\n");
+    }
+
+    public String toFileString(){
+        return id+","+title+","+isDone;
+    }
+
+    public static Task fromFileString(String line){
+        String[] parts = line.split(",");
+        int id = Integer.parseInt(parts[0]);
+        String title = parts[1];
+        boolean done = Boolean.parseBoolean(parts[2]);
+        return new Task(id,title,done);
     }
 }
--- easy/TaskManager.java
+++ easy/TaskManager.java
@@ -1,35 +1,27 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+
 
 public class TaskManager {
     private ArrayList<Task> tasks;
-    private int taskId;
+    private final String FILE_NAME = "tasks.txt";
 
     public TaskManager() {
         tasks = new ArrayList<>();
-        taskId = 1;
+        loadTasksFromFile();
     }
 
     public void addTask(String title) {
-        Task newTask = new Task(taskId, title);
+        Task newTask = new Task(title);
         tasks.add(newTask);
-        System.out.println("Task is added with the ID of :" + taskId);
-        taskId++;
+        saveTaskToFile();
+        System.out.println("Task is added.");
     }
 
     public void removeTask(int taskId) {
-        boolean found = false;
-
-        for (Task task : tasks) {
-            if (task.getId() == taskId) {
-                tasks.remove(task);
-                found = true;
-                System.out.println("Task with the ID " + taskId + " has been removed.");
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("Task not found !!!");
-        }
+        tasks.removeIf(t -> t.getId() == taskId);
+        saveTaskToFile();
+        System.out.println("Task removed.");
     }
 
     public void markAsDone(int taskId) {
@@ -37,6 +29,7 @@ public class TaskManager {
         for (Task task : tasks) {
             if (task.getId() == taskId) {
                 task.Done();
+                saveTaskToFile();
                 System.out.println("Task " + taskId + " marked as completed.");
                 found = true;
                 break;
@@ -55,6 +48,36 @@ public class TaskManager {
             for (Task task : tasks) {
                 task.getDetails();
             }
+        }
+    }
+
+    public void saveTaskToFile(){
+        try(FileWriter fw = new FileWriter(FILE_NAME)){
+            for(Task task:tasks){
+                fw.write(task.toFileString()+"\n");
+            }
+        }catch(IOException e){
+            System.out.println("Error saving tasks: "+e.getMessage());
+        }
+    }
+    private void loadTasksFromFile(){
+        File file = new File(FILE_NAME);
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] parts = line.split(",");
+                if(parts.length == 3){
+                    int id = Integer.parseInt(parts[0]);
+                    String title = parts[1];
+                    boolean isDone = Boolean.parseBoolean(parts[2]);
+                    Task t = new Task(id, title, isDone);
+                    tasks.add(t);
+                }
+            }
+            System.out.println("Tasks Loaded from saved file");
+        }catch(IOException e){
+            System.out.println("Error loading tasks: "+e.getMessage());
         }
     }
 }
